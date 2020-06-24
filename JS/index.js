1daec583b7722b9e9899594c4edf165058b1957a
Kajal Singh <singh.kajal940@gmail.com>
2020-06-24 22:13:08 +0100

    Initial commit Form Js added and toggle

---
+++ JS/index.js
@@ -0,0 +1,76 @@
+// if (Modernizr.touch) {
+
+// }
+// else {
+//     //Smoothe Scroll
+//     // A $( document ).ready() block.
+//     $(document).ready(function () {
+//         $("body").niceScroll({
+//             scrollspeed: 400,
+//             cursorcolor: "#14ffec",
+//             cursorwidth: "10px"
+//         });
+//     });
+
+//     //SCROLL BEHAVIOUR SMOOTHE
+//     $(document).ready(function () {
+//         // Add smooth scrolling to all links
+//         $(".scroll-btn, .navigation__link, .footer__link").on('click', function (event) {
+
+//             // Make sure this.hash has a value before overriding default behavior
+//             if (this.hash !== "") {
+//                 // Prevent default anchor click behavior
+//                 event.preventDefault();
+
+//                 // Store hash
+//                 var hash = this.hash;
+
+//                 // Using jQuery's animate() method to add smooth page scroll
+//                 // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+//                 $('html, body').animate({
+//                     scrollTop: $(hash).offset().top
+//                 }, 2000, function () {
+
+//                     // Add hash (#) to URL when done scrolling (default click behavior)
+//                     window.location.hash = hash;
+//                 });
+//             } // End if
+//         });
+//     });
+
+// }
+
+
+
+
+// VERSION - 2
+
+//CheckBox-Nav Collapse on Click
+$(".navigation__link").click(function () {
+    $(".navigation__checkbox").prop("checked", false);
+})
+
+// $(document).ready(function() {
+
+//     if (Modernizr.touch) {
+
+//     }
+//     else {
+//         //SCROLL ANIMATION  
+//         //*SEE HTML* *None rn- Only CSS Smooth*
+
+//         // Smooth-Scrolling
+//         $(function () {
+//             $("body").niceScroll({
+//                 cursorcolor: "#14ffec",
+//                 cursorwidth: "1rem",
+//                 cursorborder: "none",
+//                 cursorborderradius: "2px",
+//                 scrollspeed: 300,
+//                 mousescrollstep: 60,
+//                 background: "#212121"
+//             });
+//         });
+//     }
+
+// });
